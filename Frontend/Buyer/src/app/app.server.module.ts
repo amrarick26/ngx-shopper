83e4c15b48bf501f60763cd46354c7e9b37105d5
Alexa Rarick <ararick@four51.com>
2019-11-06 15:54:23 -0600

    WIP ssr

--- Frontend/Buyer/src/app/app.server.module.ts
+++ Frontend/Buyer/src/app/app.server.module.ts
@@ -4,9 +4,13 @@ import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader'
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { CookieBackendService, CookieService } from '@gorniv/ngx-universal';
 
 @NgModule({
   imports: [AppModule, ServerModule, ModuleMapLoaderModule],
   bootstrap: [AppComponent],
+  providers: [
+    { provide: CookieService, useClass: CookieBackendService }
+  ]
 })
 export class AppServerModule {}
