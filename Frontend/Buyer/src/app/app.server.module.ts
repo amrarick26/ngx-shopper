76551154c9d505391a1d18f92d96c53989448eaa
Alexa Rarick <ararick@four51.com>
2019-11-06 16:05:26 -0600

    add cookiebackendservice to app server module

--- Frontend/Buyer/src/app/app.server.module.ts
+++ Frontend/Buyer/src/app/app.server.module.ts
@@ -4,13 +4,21 @@ import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader'
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
-import { CookieBackendService, CookieService } from '@gorniv/ngx-universal';
+import { CookieBackendService, CookieService, NgxRequest, NgxResponce } from '@gorniv/ngx-universal';
 
 @NgModule({
   imports: [AppModule, ServerModule, ModuleMapLoaderModule],
   bootstrap: [AppComponent],
   providers: [
-    { provide: CookieService, useClass: CookieBackendService }
+    { provide: CookieService, useClass: CookieBackendService },
+    {
+      provide: NgxRequest,
+      useValue: { cookie: '', headers: {} },
+    },
+    {
+      provide: NgxResponce,
+      useValue: { cookie: '', headers: {} },
+    },
   ]
 })
-export class AppServerModule {}
+export class AppServerModule { }
